6d2d1bcda81f4cf01cec150113b9d56b86ba9b92
Sean Hogan <shogun70@gmail.com>
2009-04-02 03:55:25 +0000

    Implement --wrapper instead of --stringparam WRAPPER_URL

--- source/htmlwrap.pl
+++ source/htmlwrap.pl
@@ -3,14 +3,43 @@ use Cwd;
 $PWD = getcwd();
 $XSLTPROC = "/usr/bin/xsltproc --novalid --nonet";
 $TEMPLATE = "$0.xsl";
-$SRC = $ARGV[$#ARGV];
+$PARAMS = {};
+$WRAPPER = "";
+$SRC = "";
+
+my $usage = "$0 [--wrapper wrapperURL] file\n";
+
+my $n = scalar @ARGV;
+for (my $i=0; $i<$n; $i++) {
+	my $arg = $ARGV[$i];
+	if ("--help" eq $arg || "-?" eq $arg) {
+		print STDERR $usage;
+		exit 1;
+	}
+	elsif ("--wrapper" eq $arg) {
+		my $uri = $ARGV[++$i];
+		$WRAPPER = $uri;
+		next;
+	}
+	elsif ($arg =~ /^-.+/) {
+		print STDERR "Illegal option " . $arg . "\n" . "Usage:" . $usage;
+		exit 1;
+	}
+	
+	else {
+		if (!$SRC) { $SRC = $arg; }
+		else {
+			print STDERR "Cannot process more than one file.\nUsage: " . $usage;
+			quit();
+		}
+	}
+}
+
 $SRCPATH = `dirname $SRC`;
 chomp $SRCPATH;
-$PARAMS = "";
-$n = $#ARGV - 1;
-for $i (0 .. $n) {
-	$ARG = $ARGV[$i];
-	$PARAMS .= ("" eq $ARG || $ARG =~ /\s/) ? "'$ARG'" : $ARG;
-	$PARAMS .= " ";
-}
-system("$XSLTPROC --path $SRCPATH --path $PWD $PARAMS $TEMPLATE $SRC");
+
+$OUTARGS = "";
+$OUTARGS .= "--stringparam WRAPPER_URL \"" . $WRAPPER . "\" ";
+
+print STDERR "$XSLTPROC --path $SRCPATH --path $PWD $OUTARGS $TEMPLATE $SRC";
+system("$XSLTPROC --path $SRCPATH --path $PWD $OUTARGS $TEMPLATE $SRC");
