d53abd18d7981bf5249d5d2985b6f6bc136959ae
Sean Hogan <shogun70@gmail.com>
2012-02-16 10:52:42 +1100

    License

--- HTMLDecor.js
+++ HTMLDecor.js
@@ -1,5 +1,6 @@
-/*
+/*!
  * Copyright 2009-2012 Sean Hogan (http://meekostuff.net/)
+ * Mozilla Public License v2.0 (http://mozilla.org/MPL/2.0/)
  */
 
 // TODO Move script and options detection outside of the decorSystem module
